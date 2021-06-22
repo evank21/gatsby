7e2539616c0959eef7af66358d4163e0d9d9f7e7
Sam Slotsky <sam.slotsky@gatsbyjs.com>
2021-06-22 09:34:21 -0500

    Break variants into separate operation

--- packages/gatsby-source-shopify/src/gatsby-node.ts
+++ packages/gatsby-source-shopify/src/gatsby-node.ts
@@ -55,6 +55,7 @@ async function sourceAllNodes(
 ): Promise<void> {
   const {
     createProductsOperation,
+    createProductVariantsOperation,
     createOrdersOperation,
     createCollectionsOperation,
     finishLastOperation,
@@ -62,7 +63,7 @@ async function sourceAllNodes(
     cancelOperationInProgress,
   } = createOperations(pluginOptions, gatsbyApi)
 
-  const operations = [createProductsOperation]
+  const operations = [createProductsOperation, createProductVariantsOperation]
   if (pluginOptions.shopifyConnections?.includes(`orders`)) {
     operations.push(createOrdersOperation)
   }
@@ -107,6 +108,7 @@ async function sourceChangedNodes(
 ): Promise<void> {
   const {
     incrementalProducts,
+    incrementalProductVariants,
     incrementalOrders,
     incrementalCollections,
     finishLastOperation,
@@ -126,7 +128,11 @@ async function sourceChangedNodes(
       .forEach(node => gatsbyApi.actions.touchNode(node))
   }
 
-  const operations = [incrementalProducts(lastBuildTime)]
+  const operations = [
+    incrementalProducts(lastBuildTime),
+    incrementalProductVariants(lastBuildTime),
+  ]
+
   if (pluginOptions.shopifyConnections?.includes(`orders`)) {
     operations.push(incrementalOrders(lastBuildTime))
   }
